3258c1b3b77fdf30ad1b8fc156a073dc168b22a3
Jamal Turner <jlsmitty2273@yahoo.com>
2019-10-28 13:25:10 -0400

    most of the scrapper is done

--- lib/the_comfartable_gamer/scrapper.rb
+++ lib/the_comfartable_gamer/scrapper.rb
@@ -1,1 +1,21 @@
-sadfasdfs
+
+def create article
+    html = File.read('https://gameranx.com/updates/')
+    article = Nokogiri::HTML(html)
+
+    article.css("#content").each do |post|
+        #this contains the list of articles
+        # title = article.css("h1")
+        # date = article.css("time")
+        # link = article.css("")
+        post = article.css("article")
+        link = article.css(".article-link")
+
+
+
+
+end
+
+def create post(link)
+    post = link.css("p") 
+end
