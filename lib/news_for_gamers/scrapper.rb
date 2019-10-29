7ec86fbcd03ca5e6feefcf3a8b838bc1ffc18025
Jamal Turner <jlsmitty2273@yahoo.com>
2019-10-29 12:54:31 -0400

    Finally got the link working

--- lib/news_for_gamers/scrapper.rb
+++ lib/news_for_gamers/scrapper.rb
@@ -6,23 +6,23 @@ require "pry"
 def create_article
     html = open('https://www.pcgamer.com/news/')
     article = Nokogiri::HTML(html)
-
-    article.css("div.listingResult.small").each do |post|
+    #binding.pry
+    article.css("div.listingResult.small:not(.sponsored-post)").each do |post|
         #this contains the list of articles
         # title = article.css("h1")
         # date = article.css("time")
         # link = article.css("")
         articles = post.css("article").text
         link = post.css("a").first.attr("href")
-        puts post.text.split("\n")
+        puts post.text.split("\n").first
         puts link
         
     end
 
-    return create_article
+    
 
 end
-
+create_article
 
  
 
@@ -31,3 +31,4 @@ end
 #     #post = link.css("p") 
 #     end
 # end
+#content > section > div > div.listingResult.small.result2
